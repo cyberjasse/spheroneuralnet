7b868337b7b3b81fd4e03b241005319adb44fc4c
Jason B <j.bury@hotmail.fr>
2016-12-15 17:21:02 +0100

    First implementation of a Layer

---
+++ cpp/neuronnet/src/Layer.cpp
@@ -0,0 +1,29 @@
+#include "Layer.hpp"
+
+Layer::Layer(Neuron* neurons, layerSize_t length, double step){
+	ns = neurons;
+	size = length;
+	meta = step;
+}
+
+void Layer::compute(double input[], double output[]){
+	lastInput = input;
+	for(layerSize_t i=0 ; i<size ; i++){
+		output[i] = ns[i].compute(input);
+	}
+}
+
+void Layer::backpropagation(double nextLayerContributions[], double thisContributions[]){
+		//start with all thisContributions to 0
+		for(neuronSize_t j=0 ; j<ns[0].N ; j++){
+			thisContributions[j] = 0.0;
+		}
+		//
+		double contribs[ns[0].N];
+		for(layerSize_t i=0 ; i<size ; i++){
+			ns[i].backpropagation( nextLayerContributions[i], lastInput, meta, contribs);
+			for(neuronSize_t j=0 ; j<ns[0].N ; j++){
+				thisContributions[j] += contribs[j];
+			}
+		}
+}
--- cpp/neuronnet/src/Layer.hpp
+++ cpp/neuronnet/src/Layer.hpp
@@ -5,40 +5,48 @@
 #ifndef LAYER_HPP
 #define LAYER_HPP
 #include "Neuron.hpp"
-
+typedef  unsigned int layerSize_t;
 class Layer{
 	protected:
 		/** The last input vector*/
-		double lastInput[];
+		double* lastInput;
+		/** The negative of the gradient step*/
+		double meta;
 		
 	public:
 		/** Neurons in this layer*/
-		final Neuron neurons[];
+		Neuron* ns;
 		/** The number of neurons*/
-		final uint16_t size;
+		layerSize_t size;
 
 		/**
 		 * @param neurons all neurons in this layer
+		 * @param length The number of neurons
+		 * @param step The negative of the gradient step (-eta)
 		 */
-		Layer(Neuron neurons[]);
+		Layer(Neuron* neurons, layerSize_t length, double step);
 
 		/**
 		 * Compute the activation function off all neurons
 		 * @param input The input for all neurons
-		 * @return The output of all neurons
+		 * @param output The table where the output for all neurons will be stored
 		 */
-		double[] compute(double input[]);
+		void compute(double input[], double output[]);
 
 		/**
 		 * @brief Perform the backpropagation throught this layer.
 		 * @param nextLayerContributions The error contributions from the next layer
-		 * @return The error contributions of this layer
+		 *  Let Q the quadratic error, let f[i] the activation function of the ith neurons of this layer,
+		 *  nextLayerContributions = the partial derivate dQ/df[i]
+		 * @return The error contributions of this layer.
+		 *  Let Q the quadratic error, let f[j] the activation function of the jth neurons of the previous layer,
+		 *  the return = the partial derivate dQ/df[j]
 		 */
-		double[] backpropagation(double nextLayerContributions[]);
+		void backpropagation(double nextLayerContributions[], double thisContributions[]);
 
 		/**
 		 * @brief Get the last input vector given in compute()
 		 */
-		double[] getLastInput();
-}
+		double* getLastInput();
+};
 #endif
