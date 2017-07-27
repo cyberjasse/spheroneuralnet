bae0e39fa45941bf9cbed34a900f887d8f7295cd
Jason Bury <j.bury@hotmail.fr>
2017-07-27 16:19:32 +0200

    Modified learn.cpp to ride sphero if pointlist is provided

--- cpp/learn.cpp
+++ cpp/learn.cpp
@@ -3,9 +3,12 @@
  */
 #include "commander/LearningCommander.hpp"
 #include "commander/SimpleAdapter.hpp"
+#include "commander/PointlistTarget.hpp"
 #include "neuronnet/src/RBFnet.hpp"
 #include <iostream>
 #include <stdlib.h>
+#include "sphero/bluetooth/bluez_adaptor.h"
+#include "sphero/Sphero.hpp"
 
 int main(int argc, char* argv[]){
 	//test if parameters are here
@@ -16,10 +19,33 @@ int main(int argc, char* argv[]){
 	//Get parameters
 	std::string filename = argv[1];
 	int niteration = atoi( argv[2]);
+	Sphero *sph;
+	if(argc>3){
+		const std::string DEFAULT_ADDRESS = "68:86:E7:00:58:83";
+		sph = new Sphero(DEFAULT_ADDRESS.c_str(), new bluez_adaptor());
+	}
+	else{
+		sph = NULL;
+	}
 	//Create a LearningCOmmander
 	SimpleAdapter *adapter = new SimpleAdapter();
 	RBFnet *net = new RBFnet(6,2, 20, 250, -0.4);
-	LearningCommander commander = LearningCommander(NULL, adapter, net);
+	LearningCommander *commander = new LearningCommander(sph, adapter, net);
 	//start training
-	commander.learnFromFile(filename, niteration);
+	commander->learnFromFile(filename, niteration);
+	//if target file, drive
+	std::string targetname = argv[3];
+	PointlistTarget *target = new PointlistTarget(targetname);
+	commander->setTarget(target);
+	if(sph->connect()){
+		std::cerr << " >connect() == true< " << std::endl;
+	}
+	else{
+		std::cerr << " >connection failed< " << std::endl;
+		delete sph;
+		return 1;
+	}
+	const short frequency  = 5;
+	sph->addStreamObserver(commander);
+	sph->startStream(frequency);
 }
