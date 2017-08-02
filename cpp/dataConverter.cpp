aa375200a7738f99d756f4a159e04ca7d4d66af5
Jason Bury <j.bury@hotmail.fr>
2017-08-02 16:12:17 +0200

    DataConverter convert accelerometer values

--- cpp/dataConverter.cpp
+++ cpp/dataConverter.cpp
@@ -22,22 +22,22 @@ int main(int argc, char *argv[]){
 	ofstream output;
 	output.open(outputfilename);
 	// read the first line containing column names
-	const int linesize = 8;
+	const int linesize = 10;
 	std::string garb;
 	for(int i=0 ; i<linesize ; i++){
 		input >> garb;
 	}
 	// read line by line
 	std::vector<struct InputOutput> l = std::vector<struct InputOutput>();
-	const unsigned long TIMEMIN = 150000;
-	const unsigned long TIMEMAX = 250000;
+	const unsigned long TIMEMIN = 0;
+	const unsigned long TIMEMAX = 1000000;
 	int speed;
 	int head;
 	bool still = true;
 	while(still){
 		struct StreamFrame frame;
 		if(input >> frame.yaw){
-			input >> frame.x >> frame.y >> frame.speedx >> frame.speedy >> frame.chrono;
+			input >> frame.x >> frame.y >> frame.speedx >> frame.speedy >> frame.ax >> frame.ay >> frame.chrono;
 			input >> speed >> head;
 			if(frame.chrono>=TIMEMIN && frame.chrono<=TIMEMAX){//filter
 				struct InputOutput io = {frame,(uint8_t)(speed),(int16_t)(head)};
@@ -54,6 +54,8 @@ int main(int argc, char *argv[]){
 	output << "@attribute currentSpeedy numeric\n";
 	output << "@attribute targetx numeric\n";
 	output << "@attribute targety numeric\n";
+	output << "@attribute accelx numeric\n";
+	output << "@attribute accely numeric\n";
 	output << "@attribute speed numeric\n";
 	output << "@data\n";
 	for(int i=0 ; i<l.size()-1 ; i++){
@@ -61,7 +63,7 @@ int main(int argc, char *argv[]){
 		speed = adapter->normalizeSpeed(l[i].speedCommand);
 		head = adapter->normalizeHead( l[i].headCommand);
 		// WRITE HERE
-		output << tframe.currentSpeedx <<","<< tframe.currentSpeedy <<","<< tframe.targetx <<","<< tframe.targety <<","
+		output << tframe.currentSpeedx <<","<< tframe.currentSpeedy <<","<< tframe.targetx <<","<< tframe.targety <<","<< tframe.currentAccelx <<","<< tframe.currentAccely<<","
 		<< speed <<std::endl;
 	}
 	output.close();
